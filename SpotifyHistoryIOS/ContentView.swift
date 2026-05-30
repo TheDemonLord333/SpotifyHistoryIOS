089ff491e45287a6249ea8cc274c147a89d09f03
TheDemonLord333 <thedemonlord333@gmail.com>
2026-05-30 18:58:19 +0200

    Initial Commit

---
+++ SpotifyHistoryIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyHistoryIOS
+//
+//  Created by David Martens on 30.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyHistoryIOS/SpotifyHistoryIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyHistoryIOSApp.swift
+//  SpotifyHistoryIOS
+//
+//  Created by David Martens on 30.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyHistoryIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
